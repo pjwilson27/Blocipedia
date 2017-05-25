4c1e5f679658109b1ff1571793453df183bf160b
Phil Wilson <pjwilson27@gmail.com>
2017-05-25 13:41:02 +0000

    Completed CP6

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+10.times do
+    User.create!(
+        email: Faker::Internet.email,
+        password: Faker::Internet.password(6),
+        role: Faker::Number.between(0,2),
+        confirmed_at: Time.now
+    )
+end
+
+20.times do
+    Wiki.create!(
+        title: Faker::GameOfThrones.character,
+        body: Faker::Hipster.paragraph
+    )
+end
+    
+puts "#{User.count} new users created"
+puts "#{Wiki.count} new wikis created"
