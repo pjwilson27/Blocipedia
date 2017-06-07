661ebba4ee135d762682d0facb54d04cb825aa38
Phil Wilson <pjwilson27@gmail.com>
2017-06-07 19:47:05 +0000

    Completed CP9

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,2 +1,6 @@
 module WikisHelper
+    def markdown(text)
+        extensions = [:tables, :fenced_code_blocks, :autolink, :strikethrough, :underline, :highlight, :quote, :superscript, :footnotes]
+        Markdown.new(text, *extensions).to_html.html_safe
+    end
 end
